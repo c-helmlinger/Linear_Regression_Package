3e098bb18502c7582d25d3e94170b6e8f42c9a07
c-helmlinger <86612309+c-helmlinger@users.noreply.github.com>
2021-07-15 08:05:50 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name='Linear_Regression_Package',
+  author='Jessica Zhang',
+  version='1.0.0',
+  description='',
+  packages=['Linear_Regression'],
+)
+  
